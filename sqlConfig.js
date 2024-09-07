a398caadfabd2e21687dc5c5ff9f6bdd54baa935
Jirapat Tanuwoharn <jirapat_ta66@live.rmutl.ac.th>
2024-09-07 18:05:05 +0700

    editfull

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,9 +1,9 @@
 var dbconfig = {
     development: {
-        server: '192.168.56.101',
-        database:'team5_web_labDB',
+        server: '192.168.56.105',
+        database:'team7_web_labDB',
         user:'sa',
-        password:'Aa445566',
+        password:'Fifa162545',
         port: 1433,
         options:{
             encript: true,
@@ -16,7 +16,7 @@ var dbconfig = {
     },
     production: {
         server: '10.21.43.203',
-        database:'team5_web_labDB',
+        database:'team7_web_labDB',
         user:'sa',
         password:'P@ssw0rd',
         port: 1433,
